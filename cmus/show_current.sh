27e975d6ac7ae35406b41926572436adf7be3e30
Arturo Volpe <arturovolpe@gmail.com>
2014-11-09 13:45:14 -0300

    Acutalizar titulo de popup de Cmus show_current
    Se modifica el script para que muestre la hora actual aparte del música
    que se esta escuchando actualmente.

--- cmus/show_current.sh
+++ cmus/show_current.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
-cmus-remote -Q | head -2 | tail -1 | sed -e 's/.*\///' -e 's/\..*//' | xargs -0 -I{} kdialog --title "Cmus" --passivepopup {} 5
+date=`date +%d-%m\ %H:%M:%S`
+
+cmus-remote -Q | head -2 | tail -1 | sed -e 's/.*\///' -e 's/\..*//' | xargs -0 -I{} kdialog --title "$date" --passivepopup {} 2
