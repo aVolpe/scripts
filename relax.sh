8f83436c54ac36293801612c063d77c84da2cba0
Arturo Volpe <arturovolpe@gmail.com>
2015-05-09 23:05:57 -0400

    Relax - Add download option

--- relax.sh
+++ relax.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Script que ejecuta música de fondo
+# Script que ejecuta música de fondo, utilizando mplayer.
+
+# Para descargar la música es requerido:
+# * youtube-dl
+# * wget
 
 # Parámetros
-# download: Descarga la música y la aloja en ~/Music/
+# download: 
+#   Descarga la música y la aloja en ~/Music/
 # pause: 
 #   Si no se esta ejecutando: lo ejecuta
 #   Si se ejecuta lo pausa
@@ -17,6 +22,8 @@
 PIPE_JAZZ="/tmp/relax_jazz"
 PIPE_FIRE="/tmp/relax_fireplace"
 PIPE_RAIN="/tmp/relax_rain"
+
+MUSIC_HOME=~/Music
 
 function is_running () {
     if [ -p "$PIPE_JAZZ" ]; then
@@ -55,9 +62,9 @@ function start() {
     create_pipe "$PIPE_FIRE"
     create_pipe "$PIPE_RAIN"
 
-    mplayer ~/Music/jazz.mp3      -slave -input file="$PIPE_JAZZ" -loop 0 -volume 50  < /dev/null >/dev/null 2>&1 &
-    mplayer ~/Music/fireplace.mp3 -slave -input file="$PIPE_FIRE" -loop 0             < /dev/null >/dev/null 2>&1 &
-    mplayer ~/Music/rain.mp4a     -slave -input file="$PIPE_RAIN" -loop 0             < /dev/null >/dev/null 2>&1 &
+    mplayer "$MUSIC_HOME/jazz.mp3"      -slave -input file="$PIPE_JAZZ" -loop 0 -volume 50  < /dev/null >/dev/null 2>&1 &
+    mplayer "$MUSIC_HOME/fireplace.mp3" -slave -input file="$PIPE_FIRE" -loop 0             < /dev/null >/dev/null 2>&1 &
+    mplayer "$MUSIC_HOME/rain.m4a"      -slave -input file="$PIPE_RAIN" -loop 0             < /dev/null >/dev/null 2>&1 &
 
 }
 
@@ -87,6 +94,17 @@ function usage() {
  
 }
 
+function download() {
+ 
+    echo "Downloading rain"
+    wget -O "$MUSIC_HOME/rain.m4a" "http://174.36.223.28/audio1110/0.m4a"
+    echo "Downloading fireplace"
+    youtube-dl  --audio-quality 0 -x --audio-format mp3 "https://www.youtube.com/watch?v=DIx3aMRDUL4" -o "$MUSIC_HOME/fireplace.mp3"
+    echo "Downloading jazz"
+    youtube-dl  --audio-quality 0 -x --audio-format mp3 "https://www.youtube.com/watch?v=HMnrl0tmd3k" -o "$MUSIC_HOME/jazz.mp3"
+    echo "Download complete, run sh relax.sh start"
+}
+
 opt="$1"
 case "$opt" in
     "")
@@ -97,6 +115,8 @@ case "$opt" in
         stop;;
     "pause")
         pause;;
+    "download")
+        download;;
 esac
 
 
